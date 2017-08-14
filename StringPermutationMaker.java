a7793361944a99d7575e21a85692b55676c66fd3
Priyabrata Rout <30842804+PriyabrataRout@users.noreply.github.com>
2017-08-14 16:58:49 +0530

    Update StringPermutationMaker.java
    Added comments and finally understood how it works
           ABC
    AB+C    AC+B   BC+A
    -------   ------  -------
    ABC       ACB     BCA
    BAC      CAB      CBA

--- StringPermutationMaker.java
+++ StringPermutationMaker.java
@@ -2,17 +2,17 @@ package com.priya.CrackingTheCodingInterview;
 
 public class StringPermutationMaker {
 
-	private static void permutationmaker(String string, String prifix) {
-		if (string.length() == 0) {
-			System.out.println("'" + prifix + "'");
+	private static void permutationmaker(String input, String result) {
+		if (input == "" || input.isEmpty()) {
+			System.out.println(result);
 		} else {
-			for (int i = 0; i < string.length(); i++) {
-				String rem = string.substring(0, i) + string.substring(i + 1);
-				System.out.println(" rem = " + string.substring(0, i) + " ** " + string.substring(i + 1) + " *** " + i);
-				System.out.println(rem + " -- " + prifix + "+" + string.charAt(i));
-				System.out.println("----------------------" + string);
-				permutationmaker(rem, prifix + string.charAt(i));
-
+			/**
+			 * Remove one charecter from string and rest as reminder. Add the
+			 * selected value and result. You will get the ans
+			 **/
+			for (int i = 0; i < input.length(); i++) {
+				String reminder = input.substring(0, i) + input.substring(i + 1);
+				permute(reminder.trim(), result + input.charAt(i));
 			}
 
 		}
